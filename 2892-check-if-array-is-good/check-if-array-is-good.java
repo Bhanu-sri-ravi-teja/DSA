68d9025a36164feece039759f1c11357cf95ce96
A BHANUSRI RAVI TEJA <23p31a05d7@acet.ac.in>
2026-05-18 13:27:45 +0530

    Time: 4 ms (43.56%) | Memory: 45 MB (33.15%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,29 +1,19 @@
 class Solution {
     public boolean isGood(int[] nums) {
+        
+        
         Arrays.sort(nums);
-        int max = nums[nums.length - 1];
-        if (nums.length != (max + 1)) {
+        
+        int temp = 1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] != temp){
+                return false;
+            }
+            temp++;
+        }
+        if(nums[nums.length-1] != --temp){
             return false;
         }
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }
-            else {
-                map.put(nums[i], 1);
-            }
-        }
-        List<Integer> list = new ArrayList<>(map.values());
-        int count = 0;
-        for (int num : list) {
-            if (num >= 2) {
-                count++;
-            }
-        }
-        if (map.get(max) == 2 && (count == 1)) {
-            return true;
-        }
-        return false;
+        return true;
     }
 }
